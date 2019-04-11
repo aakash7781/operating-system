efbe0014e793947ff8a9be0d131439e0e7baac07
aakash7781 <46555006+aakash7781@users.noreply.github.com>
2019-04-11 12:42:22 +0530

    Add files via upload

---
+++ fcfsDiskManagement.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int x,res=0;
+	cout<<"enter cylinder numbers\n";
+	cin>>x;
+	int a[x+1];
+	for(int i=1;i<=x;i++)
+	{
+		cout<<"enter cylinder numberings for cylinder ("<<(i)<<"): ";
+		cin>>a[i];
+	}
+	cout<<"enter current cylinder number\n";
+    int var=0;
+	cin>>a[var];
+	cout<<"total disk = ";
+    int i=1;
+    while(i<=x)
+	{
+		if(a[var]>a[i])
+			res=res+a[var]-a[i];
+		else
+			res=res+a[i]-a[var];
+		var++;
+		i++;
+	}
+	cout<<res;
+	
+}
